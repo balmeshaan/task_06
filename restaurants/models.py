02a5dae75fb981d4fe7f50a1214f379f128bc0b9
barak <balmeshaan@gmail.com>
2019-10-03 19:51:04 +0300

    task06 first commit

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+
+    def get_absolute_url(self):
+    	return reverse('restaurant-detail', kwargs={'restaurant_id':self.id})
